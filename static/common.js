6665ccae60fac9231deb2cd7150d0111d25a57df
user <user@localhost.localdomain>
2012-02-27 01:24:37 +0900

    toctree, contentsを折り畳み表示。

--- static/common.js
+++ static/common.js
@@ -1,4 +1,7 @@
 ;jQuery(function() {
-    $('.treeview').treeview();
+    $('.toctree-wrapper ul:not(:has("li")), .contents ul:not(:has("li")), .treeview ul:not(:has("li"))').remove();
+    $('.toctree-wrapper > ul, .contents > ul, .treeview').treeview({
+        collapsed: true
+    });
     $('table').tableHover({colClass: 'hover'});
 });
